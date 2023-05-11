ee562b2f2e25f26f4d2e3ef5ed407012f606cbf8
MKN2003 <kazimovmiraziz197@gmail.com>
2023-05-11 23:40:25 +0500

    Changes have been made

--- src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task3;
 
 public class AliquotNumbers {
     public void isFirstAliquot(int first, int second) {
+        int aliquot = second % first;
+
+        if (aliquot == 0) {
+            System.out.println("Aliquot");
+        }
+        else {
+            System.out.println("Not aliquot");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -1,6 +1,22 @@
 package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
-    public void calculateSalary(int salary) {
+    public void calculateSalary(float salary) {
+        if (salary > 0) {
+            if (salary <= 10000) {
+                salary = salary - (salary * 15 / 100);
+                System.out.println(salary);
+            }
+            if (salary > 10000 && salary <= 20000) {
+                salary = salary - (salary * 18 / 100);
+                System.out.println(salary);
+            }
+            if (salary > 20000) {
+                salary = salary - (salary * 20 / 100);
+                System.out.println(salary);
+            }
+        }
+        else
+            System.out.println("wrong input!");
     }
 }
